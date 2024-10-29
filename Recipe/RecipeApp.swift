beac01d67de569dc88953b1ecf65131f25d2dc74
salwadie <salwadie24@gmail.com>
2024-10-29 15:11:11 +0300

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Sara Alwadie on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecipeTests/RecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Sara Alwadie on 26/04/1446 AH.
+//
+
+import Testing
+@testable import Recipe
+
+struct RecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
